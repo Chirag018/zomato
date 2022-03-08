74d186e86b7322ccd403443f4f9e3b050d85cdaa
Chirag <56735482+Chirag018@users.noreply.github.com>
2022-03-08 14:17:05 +0530

    first commit

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='absolute-center max-width'> 
+            Made with 🔥 by Chirag
+        </div>
+    )
+}
+
+export default Footer
